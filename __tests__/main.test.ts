37dc4c52cb2beaac7fb6bf63dc7f91e1735b82a0
genrikh <genrihfetishev@yandex.ru>
2019-11-10 17:22:47 +0300

    added a few tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,4 +1,4 @@
-import { AnyAction, Reducer, Store } from "redux";
+import { AnyAction, Dispatch, Reducer, Store } from "redux";
 import { createStore, transactionWrapper } from "../src/";
 import { ThunkDispatch } from "redux-thunk";
 
@@ -33,5 +33,56 @@ describe("all tests", () => {
 
     expect(store.getState().value).toBe("third");
     expect(subscriber).toBeCalledTimes(1);
+  });
+
+  test("asynchronous transaction", async () => {
+    const subscriber = jest.fn();
+    store.subscribe(subscriber);
+
+    const dispatch = store.dispatch as ThunkDispatch<any, any, any>;
+
+    await dispatch(
+      transactionWrapper(async dispatch => {
+        await new Promise(resolve => setTimeout(() => resolve(), 10));
+        await dispatch(actionCreator("second"));
+        expect(store.getState().value).toBe("second");
+        await dispatch(actionCreator("third"));
+      })
+    );
+
+    expect(store.getState().value).toBe("third");
+    expect(subscriber).toBeCalledTimes(1);
+  });
+
+  test("complex transaction", async () => {
+    const subscriber = jest.fn();
+    store.subscribe(subscriber);
+
+    const dispatch = store.dispatch as ThunkDispatch<any, any, any>;
+
+    await dispatch(
+      async (
+        dispatch: ThunkDispatch<any, any, any>,
+        getState: () => typeof preloadState
+      ) => {
+        const { value } = getState();
+        expect(value).toBe("third");
+
+        await dispatch(actionCreator("first"));
+
+        await dispatch(
+          transactionWrapper(async dispatch => {
+            await dispatch(actionCreator("second"));
+            await dispatch(actionCreator("third"));
+            await dispatch(actionCreator("fourth"));
+          })
+        );
+
+        await dispatch(actionCreator("fifth"));
+      }
+    );
+
+    expect(store.getState().value).toBe("fifth");
+    expect(subscriber).toBeCalledTimes(3);
   });
 });
